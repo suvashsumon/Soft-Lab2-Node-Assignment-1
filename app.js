1b3c67182e0057b0ad2cc21e73f7655ecf1a54ae
suvashkumar <suvashkumar.naogaon@gmail.com>
2022-03-08 11:49:53 +0600

    home route added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const leaderRouter = require('./routers/leaderRouter');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send("<h1>Application's Home Page</h1><br><br><b>Submitted By :</b><br>Name : Suvash Kumar<br>Roll: 1910476124");
+});
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
